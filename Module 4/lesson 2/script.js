13ab2b6f880ad8977d8755376b3fe13fefb5803b
Zulkarnaeen-coder <mehnurhafsa77@gmail.com>
2025-11-23 16:25:23 +0600

    act1

---
+++ Module 4/lesson 2/script.js
@@ -0,0 +1,7 @@
+console.log("hi"); 
+for (var i = 1; i <=1000000000000000000000000000 ; i++) {
+    var ran = Math.ceil(Math.random() * 180) +1
+    console.log(ran)
+}
+var sin = Math.sin(ran)
+console.log("The sin of " + ran + " degrees =" + sin);
