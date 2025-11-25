d16da054603ab9eec29948697b48b55e83834998
Zulkarnaeen-coder <mehnurhafsa77@gmail.com>
2025-11-25 17:17:43 +0600

    acp

---
+++ Module 4/lesson 6/scriptacp.js
@@ -0,0 +1,20 @@
+function tinuscolor(params) {
+    
+}
+    var color = ["pink", "white", "blue", 'green', 'red', 'purple', 'orange',]
+    document.getElementById('list').
+        innerHTML += color.join("-");
+    document.getElementById("a1").
+        innerHTML += color[0];
+    document.getElementById("b2").
+        innerHTML += color[1];
+    document.getElementById("c3").
+        innerHTML += color[2];
+    document.getElementById("d4").
+        innerHTML += color[3];
+    document.getElementById("e5").
+        innerHTML += color[4];
+    document.getElementById("f6").
+        innerHTML += color[5];
+    document.getElementById("g7").
+        innerHTML += color[6];
